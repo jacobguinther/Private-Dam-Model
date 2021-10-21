06423f8fe413ee25869d404ab195ca47a47d7d73
Jacob Guinther <jacob@block.science>
2021-10-21 09:48:10 -0600

    Update labs.py

--- labs.py
+++ labs.py
@@ -1,7 +1,2 @@
-
-# Example labs.py
-# Import the file that defines our instance of the Experiment class (exp)
- from models.config import exp
-​
-# The root directory of our model code
-model_dir = "demos"
+from models.config import exp
+model_dir = 'models'
